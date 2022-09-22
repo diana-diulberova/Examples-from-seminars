8984794fed58990dd1bf83ae42b9a71c4a1fded6
Diana <d.dyulberova@gmail.com>
2022-09-22 15:07:35 +0500

    произведение чисел от 1 до N

---
+++ task28/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+: Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+тоже самое что с суммой, только умножение
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int GetComNums(int number)
+{
+    int com = 1;
+    int i = 1;
+    while(i <= number)
+    {
+        com = com*i;
+        Console.Write($"{i}");
+        i++;
+    }
+    return com;
+}
+
+Console.WriteLine($" Произведение чисел от 1 до {userNumber} = {GetComNums(userNumber)}");
