575340d8694f0da580043dd2bccda2e8864092c2
Diana <d.dyulberova@gmail.com>
2022-09-23 19:36:03 +0500

    проверяет существование треугольника со сторонами заданной длины

---
+++ task40/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход три числа и проверяет, 
+может ли существовать треугольник с сторонами такой длины
+*/
+
+Console.WriteLine("Введите длину первой стороны треугольника: ");
+int firstSide = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину второй стороны треугольника: ");
+int secondSide = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину третьей стороны треугольника: ");
+int thirdSide = Convert.ToInt32(Console.ReadLine());
+
+if(firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide && secondSide + thirdSide > firstSide)
+{
+    Console.WriteLine($"Треугольник со сторонами {firstSide}, {secondSide}, {thirdSide} существует");
+}
+else
+{
+     Console.WriteLine($"Треугольника со сторонами {firstSide}, {secondSide}, {thirdSide} НЕ существует");
+}
