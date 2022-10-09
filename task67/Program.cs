c7a771b993cdf97fbfb0bc32c06011b07e37e272
Diana <d.dyulberova@gmail.com>
2022-10-09 15:50:43 +0500

    возвращает сумму цифр заданного числа

---
+++ task67/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать на вход число и 
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int SumRec(int userNumber)
+{
+    if(userNumber == 0) return 0;
+    else return userNumber % 10 + SumRec(userNumber / 10);
+}
+
+Console.WriteLine(SumRec(userNumber));
