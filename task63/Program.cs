c75c5eda557871a0cb8c5781d39bfb17994affd8
Diana <d.dyulberova@gmail.com>
2022-10-09 15:47:32 +0500

    показывает числа от 1 до N

---
+++ task63/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные 
+числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+string NumbersRec(int a, int userNumber)
+{
+    if(a < userNumber)
+    { 
+        return $"{a} " + NumbersRec(a + 1, userNumber);
+    }
+    else
+    { 
+        return $"{userNumber} ";
+    }
+}
+
+Console.WriteLine(NumbersRec(1, userNumber));
